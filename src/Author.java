d609f26f481134fd27d946b23bc77d19bb3bd125
AliciaMonsibais <alicia.monsibais1@gmail.com>
2019-08-06 11:33:21 -0500

    using suppress warning

--- src/Author.java
+++ src/Author.java
@@ -3,7 +3,7 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-
+ @SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
 
@@ -20,6 +20,7 @@ public class Author extends Person {
         return books;
     }
 
+    @SuppressWarnings("unused")
     public List<String> publishedBooks() {
         return books;
     }
@@ -28,7 +29,7 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
+//    @Override
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
